1aad32bd24ead6bc1ca42a762259d207fff8e028
rafael <rafael@xperiencexr.com>
2022-04-29 09:07:21 -0300

    update .gitignore

---
+++ src/package-example/request.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+
+//in node, axios makes use of 'http' module from node.js
+//supports Promise API -> can use async/await
+//automatic transforms for JSON data
+//can cancel requests after started, and can retrieve info on
+//  request progress
+
+
+axios.get('https://www.google.com')
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    .then(() => {
+        console.log('All done!');
+    })
