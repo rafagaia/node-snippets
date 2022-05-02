c1cbd7bd0c1f41ff354e80c06d8ed9e0b98f34af
rafael <rafael@xperiencexr.com>
2022-05-02 12:27:31 -0300

    addition of hard-coded endpoints on basic http server

--- src/http-server/index.js
+++ src/http-server/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'application/json',
     });
@@ -10,8 +10,34 @@ const server = http.createServer((req, res) => {
         id: 1,
         message: 'Response message here!'
     }));
-});
+});*/
 
+//Event Emitter
+const server = http.createServer();
+
+
+server.on('request', (req, res) => {
+    if (req.url === '/friends') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+            id: 1,
+            name: 'Jesse'
+        }));
+    } else if (req.url === '/messages') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello there</li>');
+        res.write('</ul>');
+        res.write('</body></html>');
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+});
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}.`);
