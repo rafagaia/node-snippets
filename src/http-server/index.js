b63160346962e9460309d87d481cf54b766499c5
rafael <rafael@xperiencexr.com>
2022-05-02 18:33:00 -0300

    more on pure node http server, before using express

--- src/http-server/index.js
+++ src/http-server/index.js
@@ -16,14 +16,41 @@ const PORT = 3000;
 const server = http.createServer();
 
 
+const friends = [
+    {
+      id: 0,
+      name: 'Raf',
+    },
+    {
+      id: 1,
+      name: 'Gaia',
+    },
+    {
+      id: 2,
+      name: 'Joe',
+    }
+  ];
+
 server.on('request', (req, res) => {
-    if (req.url === '/friends') {
+    const items = req.url.split('/');
+    // /friends/2 => ['', 'friends', '2']
+    // /friends/
+    if (req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+        const friend = data.toString();
+        console.log('Request:', friend);
+        friends.push(JSON.parse(friend));
+        });
+        req.pipe(res);
+    } else if (req.method === 'GET' && items[1] === 'friends') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-            id: 1,
-            name: 'Jesse'
-        }));
+        if (items.length === 3) {
+        const friendIndex = Number(items[2]);
+        res.end(JSON.stringify(friends[friendIndex]));
+        } else {
+        res.end(JSON.stringify(friends));
+        }
     } else if (req.url === '/messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
