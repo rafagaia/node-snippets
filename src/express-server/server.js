a2b5674ad2444e5a6f09595447b3526a31a3e490
rafael <rafael@xperiencexr.com>
2022-05-05 11:38:57 -0300

    added new routes

--- src/express-server/server.js
+++ src/express-server/server.js
@@ -4,6 +4,16 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+    {
+        id: 0,
+        name: 'D Brane'
+    },
+    {
+        id: 1,
+        name: 'Einstein'
+    }
+];
 
 //Routes:
 app.get('/', (req, res) => {
@@ -13,6 +23,23 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/friends', (req, res) => {
+    res.json(friends);
+});
+
+//GET /friends/21
+app.get('/friends/:id', (req, res) => {
+    const friendId = Number(req.params.id); //or +req.params.id;
+    const friend = friends[friendId];
+    if (friend) {
+        res.status(200).json(friend);
+    } else {
+        res.status(404).json({
+            error: "Friend does not exist"
+        });
+    }
+});
+
 app.get('/messages', (req, res) => {
     res.send('<h1>YOYO TETHERRR</h1>')
 });
@@ -20,7 +47,7 @@ app.get('/messages', (req, res) => {
 app.post('/messages', (req, res) => {
     console.log('POST Message');
     res.send('send it');
-})
+});
 
 
 
