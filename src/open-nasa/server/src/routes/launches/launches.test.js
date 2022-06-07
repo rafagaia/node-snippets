9b7024cb6602d2f4ae7f17db987787f1c027e2a4
rafael <rafael@xperiencexr.com>
2022-06-07 20:35:02 -0300

    test: POST /launches for 201 created

--- src/open-nasa/server/src/routes/launches/launches.test.js
+++ src/open-nasa/server/src/routes/launches/launches.test.js
@@ -2,19 +2,43 @@ const request = require('supertest');
 const app = require('../../api');
 
 describe('Test GET /launches', () => {
-    test('It should respond with status 200 success', async () => {
+    test('It should respond with status 200 success.', async () => {
         const response = await request(app)
             .get('/launches')
             .expect('Content-Type', /json/)
-            .expect(200)
+            .expect(200);
         // expect(response.statusCode).toBe(200); can use this jest way, or chain functions with supertest, as above
     });
 });
 
 describe('Test POST /launches', () => {
-    test('It should respond with status 200 success', () => {
-        const response = 200;
-        expect(response).toBe(200);
+    const completeLaunchData = {
+        mission: 'USS Liberty',
+        rocket: 'NSS 4232',
+        target: 'Mars Loc 33',
+        launchDate: 'September 7, 2022'
+    };
+
+    const launchDataWithoutDate = {
+        mission: 'USS Liberty',
+        rocket: 'NSS 4232',
+        target: 'Mars Loc 33',
+    }
+
+    test('It should respond with status 201 created.', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send(completeLaunchData)
+        .expect('Content-Type', /json/)
+        .expect(201);
+
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+
+        expect(requestDate).toBe(responseDate);
+
+        //whenever we test response body, we use jest assertions:
+        expect(response.body).toMatchObject(launchDataWithoutDate);
     });
 
     test('It should catch missing required properties', () => {
