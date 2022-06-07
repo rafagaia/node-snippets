d200a70479f2648193c41c7a27a06c51e081b0d6
rafael <rafael@xperiencexr.com>
2022-06-07 20:47:23 -0300

    test: invalid POST /launches tests

--- src/open-nasa/server/src/routes/launches/launches.test.js
+++ src/open-nasa/server/src/routes/launches/launches.test.js
@@ -25,12 +25,19 @@ describe('Test POST /launches', () => {
         target: 'Mars Loc 33',
     }
 
+    const launchDataWithInvalidDate = {
+        mission: 'USS Liberty',
+        rocket: 'NSS 4232',
+        target: 'Mars Loc 33',
+        launchDate: 'Septem'
+    };
+
     test('It should respond with status 201 created.', async () => {
         const response = await request(app)
-        .post('/launches')
-        .send(completeLaunchData)
-        .expect('Content-Type', /json/)
-        .expect(201);
+            .post('/launches')
+            .send(completeLaunchData)
+            .expect('Content-Type', /json/)
+            .expect(201);
 
         const requestDate = new Date(completeLaunchData.launchDate).valueOf();
         const responseDate = new Date(response.body.launchDate).valueOf();
@@ -41,8 +48,27 @@ describe('Test POST /launches', () => {
         expect(response.body).toMatchObject(launchDataWithoutDate);
     });
 
-    test('It should catch missing required properties', () => {
+    test('It should catch missing required properties', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(launchDataWithoutDate)
+            .expect('Content-Type', /json/)
+            .expect(400);
+
+        expect(response.body).toStrictEqual({
+            error: 'Missing required launch properties'
+        });
     });
 
-    test('It should catch invalid dates', () => {});
+    test('It should catch invalid dates', async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send(launchDataWithInvalidDate)
+            .expect('Content-Type', /json/)
+            .expect(400);
+
+        expect(response.body).toStrictEqual({
+            error: 'Invalid Launch Data'
+        });
+    });
 });
