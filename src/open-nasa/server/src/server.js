5f6abb3c671215c493f1cfc5283e1871731f216e
rafael <rafael@xperiencexr.com>
2022-06-09 16:47:09 -0300

    feat: passes Mongo connection logic from server.js onto services/mongo

--- src/open-nasa/server/src/server.js
+++ src/open-nasa/server/src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const http = require('http');
-const mongoose = require('mongoose');
+const { mongoConnect } = require('./services/mongo');
 //const io = require("socket.io");
 
 
@@ -12,27 +12,11 @@ const httpServer = http.createServer(apiServer);
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = process.env.MONGO_URL;
-
-//mongoose event emitter that emits events when connection is ready or when there's errors
-//since only triggered once, can use 'once' instead of 'on', so that event only triggers callback once.
-mongoose.connection.once('open', () => {
-    console.log('MongoDB Connection Ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error(`[MONGOOSE_ERROR] Connection Err: ${err}...`);
-});
-
 const { loadPlanetsData } = require('./models/planets.model');
 
 async function startupServer() {
-    await mongoose.connect(MONGO_URL);/*, {
-        useNewUrlParser: true, //determines how mongoose parses URL string
-        useFindAndModify: false, //disables outdated way of updating mongo data
-        useCreateIndex: true, //uses create index way instead on old ensure index
-        useUnifiedTopology: true //mongoose will use updated way of talking to clusters of mongo databases
-    }*/ //commented out because we're using Mongoose version higher than 6.
+    await mongoConnect();
+    
     await loadPlanetsData();
 
     httpServer.listen(PORT, () => {
---
+++ src/open-nasa/server/src/services/mongo.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = process.env.MONGO_URL;
+
+//mongoose event emitter that emits events when connection is ready or when there's errors
+//since only triggered once, can use 'once' instead of 'on', so that event only triggers callback once.
+mongoose.connection.once('open', () => {
+    console.log('MongoDB Connection Ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(`[MONGOOSE_ERROR] Connection Err: ${err}...`);
+});
+
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL); /*, {
+        useNewUrlParser: true, //determines how mongoose parses URL string
+        useFindAndModify: false, //disables outdated way of updating mongo data
+        useCreateIndex: true, //uses create index way instead on old ensure index
+        useUnifiedTopology: true //mongoose will use updated way of talking to clusters of mongo databases
+    }*/ //commented out because we're using Mongoose version higher than 6.
+}
+
+module.exports =  {
+    mongoConnect,
+};
