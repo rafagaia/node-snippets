5a5d96bc0a6791473a5ca39b84fd8218332ef391
rafagaia <rafaelvsgaia@gmail.com>
2023-02-15 02:02:10 -0300

    fix: update client route hooks with /v1/

--- open-nasa/client/src/hooks/requests.js
+++ open-nasa/client/src/hooks/requests.js
@@ -22,14 +22,14 @@ async function getFarmsByFruitname(fruitname /*, url*/) {
 
 //Load Planets and return as JSON
 async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`);
+  const response = await fetch(`${API_URL}/v1/planets`);
 
   return await response.json();
 }
 
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  const response = await fetch(`${API_URL}/launches`);
+  const response = await fetch(`${API_URL}/v1/launches`);
   const fetchedLaunches = await response.json();
 
   return fetchedLaunches.sort((a, b) => {
@@ -40,7 +40,7 @@ async function httpGetLaunches() {
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
   try {
-    return await fetch(`${API_URL}/launches`, {
+    return await fetch(`${API_URL}/v1/launches`, {
       method: "post",
       headers: {
         "Content-Type": "application/json",
@@ -57,7 +57,7 @@ async function httpSubmitLaunch(launch) {
 // Delete launch with given ID.
 async function httpAbortLaunch(id) {
   try {
-    return await fetch(`${API_URL}/launches/${id}`, {
+    return await fetch(`${API_URL}/v1/launches/${id}`, {
       method: "delete",
     });
   } catch (err) {
