4e74664fd0e812017b51d588e407631b99ee7937
rafagaia <rafaelvsgaia@gmail.com>
2023-02-15 02:52:36 -0300

    fix:test: add /v1/ to route urls

--- open-nasa/server/src/routes/launches/launches.test.js
+++ open-nasa/server/src/routes/launches/launches.test.js
@@ -19,7 +19,7 @@ describe('Launches API', () => {
     describe('Test GET /launches', () => {
         test('It should respond with status 200 success.', async () => {
             const response = await request(app)
-                .get('/launches')
+                .get('/v1/launches')
                 .expect('Content-Type', /json/)
                 .expect(200);
             // expect(response.statusCode).toBe(200); can use this jest way, or chain functions with supertest, as above
@@ -49,7 +49,7 @@ describe('Launches API', () => {
     
         test('It should respond with status 201 created.', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(completeLaunchData)
                 .expect('Content-Type', /json/)
                 .expect(201);
@@ -65,7 +65,7 @@ describe('Launches API', () => {
     
         test('It should catch missing required properties', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithoutDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
@@ -77,7 +77,7 @@ describe('Launches API', () => {
     
         test('It should catch invalid dates', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithInvalidDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
