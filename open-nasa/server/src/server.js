08fd0d88237c2bebd3b67240b2a65dd77e13214b
rafagaia <rafaelvsgaia@gmail.com>
2023-02-20 04:47:13 -0300

    feat: applies key.pem and cert.pem to httpsServer

--- open-nasa/server/src/server.js
+++ open-nasa/server/src/server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
-const http = require('http');
+const fs = require('fs');
+// const http = require('http');
+const https = require('https');
 const app = require('./app');
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 const { loadLaunchData } = require('./models/launches.model');
 
-const httpServer = http.createServer(app);
+// const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+    key: fs.readFileSync('secrets/key.pem'),
+    cert: fs.readFileSync('secrets/cert.pem')
+}, app);
 
 const PORT = process.env.PORT || 8000;
 
@@ -15,7 +21,7 @@ async function startupServer() {
     await loadPlanetsData();
     await loadLaunchData();
 
-    httpServer.listen(PORT, () => {
+    httpsServer.listen(PORT, () => {
         console.log(`Server Listening on Port ${PORT}...`);
     });
 
